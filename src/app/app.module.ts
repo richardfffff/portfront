6104ebaaca8fb26051906978580a3b5bac23826c
richardfffff <richardmace6@gmail.com>
2022-09-19 19:37:21 -0300

    generacion de componentes y bajada de fotos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArgprogComponent } from './components/argprog/argprog.component';
+import { HeaderComponent } from './components/header/header.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HSComponent } from './components/h-s/h-s.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { SocialRedesComponent } from './components/social-redes/social-redes.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDmiComponent } from './components/acerca-dmi/acerca-dmi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArgprogComponent,
+    HeaderComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    FooterComponent,
+    HSComponent,
+    HomeComponent,
+    LoginComponent,
+    ProyectosComponent,
+    SocialRedesComponent,
+    BannerComponent,
+    AcercaDmiComponent
   ],
   imports: [
     BrowserModule,
